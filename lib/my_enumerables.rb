74ba9855035015f878995718fedc91b7d9c37080
Oliver Jochum <oliver.jochum@yahoo.com>
2023-03-07 17:12:33 +0100

    defined and tested my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,14 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    if block_given?
+      for element in self
+        yield element
+      end
+    end
+    return self
+  end
+
+  
 end
